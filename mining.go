1560465ac2aa80e2eadfed43addf02e94ba29917
fukaoi <info@fukaoi.org>
2017-12-25 09:03:14 +0900

    modified

--- mining.go
+++ mining.go
@@ -3,17 +3,25 @@ package main
 import (
 	"os/exec"
 	"fmt"
+	"regexp"
+	"os"
+	"strconv"
 )
 
 func main() {
-	//comdstr := "lsusb | grep \"STM\" | cut -c5-7,15-18"
 	comdstr := "lsusb | grep \"ID\" | cut -c5-7,15-18"
+	usbCount := "lsusb | grep ID | wc -l"
+
 	out, err := exec.Command("sh", "-c", comdstr).Output()
+	countOut, errOut := exec.Command("sh", "-c", usbCount).Output()
 
-	//out2, _ := exec.Command("ls", "-la").Output()
-	if err == nil {
-		s := fmt.Sprintf("%s", out)
-		fmt.Println(s)
+	if err != nil || errOut != nil {
+		fmt.Println(err, errOut)
+		os.Exit(1)
 	}
-}
 
+	count, _ := strconv.Atoi(string(countOut[0]))
+	re := regexp.MustCompile("[0-9]{3}\\s[0-9]{3}\\n")
+	var result []string = re.FindAllString(string(out), count)
+	fmt.Println(result[0])
+}
