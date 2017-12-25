7d9b0976efb363f461ae0149fb4a30456dd19243
fukaoi <info@fukaoi.org>
2017-12-25 23:13:48 +0900

    modified

--- mining.go
+++ mining.go
@@ -8,12 +8,23 @@ import (
 	"strconv"
 )
 
+const (
+	CGMINER = "cgminer --gridseed-options=baud=115200,freq=800,chips=40,modules=1,usefifo=0,btc=0 --scrypt "
+	POOL = "--url=stratum+tcp://us2.litecoinpool.org:3333 --userpass=yarichin.1:1 --url=stratum+tcp://us.litecoinpool.org:3333 --userpass=yarichin.1:1 "
+)
+
+const (
+	DELIMITER = ","
+)
+
 func main() {
 	count := getUsbDeviceCount()
-	usb := getUsbDevice(count)
-	fmt.Println(usb)
-	for i := 0; i < count; i++ {
-		fmt.Println(usb[i])
+	devices := getUsbDevice(count)
+	cmdStr := createMiningCmd(devices)
+	_, err := exec.Command(cmdStr).Output()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 }
 
@@ -37,4 +48,17 @@ func getUsbDeviceCount() (count int) {
 	}
 	result, _ := strconv.Atoi(string(out[0]))
 	return result
+}
+
+func createMiningCmd(devices []string) (cmd string){
+	concatStr := "--usb="
+	for i := 0; i < len(devices); i++ {
+		split := regexp.MustCompile("\\s").ReplaceAllString(devices[i], ":")
+		concatStr += split
+		if len(devices) - 1 > i {
+			concatStr += DELIMITER
+		}
+	}
+
+	return CGMINER + POOL + concatStr
 }
